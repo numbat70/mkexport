4e974db2851feb1ebdac67a9cc52caa5faf4d898
numbat70 <numbat70@icloud.com>
2017-04-18 11:03:46 +0100

    Add files via upload

--- shippingcosts/js/calc.js
+++ shippingcosts/js/calc.js
@@ -1,9 +1,57 @@
-$("#calcTots").click(function() {
+//$("#mktest").click(function() {
+    //alert("clicked"); 
+//    var a = $("#goodsvalue").val();
+//    var b = $("#handlingcost").val();   
+//    var sum = a + b;
+//    alert(sum); 
+//});
 
-    var a = $("#goodsvalue").val();
-    var b = $("#handlingcost").val();   
-    var sum = a + b;
-    alert(sum); 
+
+$("#mktest").click(function() {
+//EX WORKS
+    var VOG=parseInt($("#VOG").val());
+    var COH=parseInt($("#COH").val());
+	var COC=parseInt($("#COC").val());
+	var HC=parseInt($("#HC").val());
+//FOB	
+    var FOB=VOG + COH + COC + HC           
+    $("#FOB").val(FOB);
+
+
+	var TC=parseInt($("#TC").val());
+	var IC=parseInt($("#IC").val());
+
+
+//CIF
+    var CIF=TC + IC  + FOB         
+    $("#CIF").val(CIF);
+
+
+
+	var COHA=parseInt($("#COHA").val());
+	var CDpercent=parseInt($("#CDpercent").val());
+    var CD=CDpercent * 500;
+    $("#CD").val(CD);
+	
+	var ITpercent=parseInt($("#ITpercent").val());
+    var IT=ITpercent * 500;
+    $("#IT").val(IT);
+
+
+//DDP
+	var DDP=CIF  + COHA + CD + IT + ICust + CCA
+    $("#DDP").val(DDP);
+
+ 	//var =parseInt($("#handlingcost").val());
+	//var =parseInt($("#handlingcost").val());
+
+
+
+    alert(FOB); 
+    ALERT(CIF);
 });
+
+
+
 
 
